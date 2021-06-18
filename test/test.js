a79d4de7ed3d3adfcf40791a03263b1a12477a77
k_taka <k_taka@gihyo.co.jp>
2021-06-18 17:48:04 +0900

    init.

---
+++ test/test.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+const md = require('markdown-it')();
+const captions = require('../index.js');
+md.use(captions);
+
+const ms = [
+  [
+    'Figure. A cat.',
+    '<p class="caption-img">Figure. A cat.</p>\n'
+  ], [
+    'Figure is a cat.',
+    '<p>Figure is a cat.</p>\n'
+  ], [
+    'Figure 1. A cat.',
+    '<p class="caption-img">Figure 1. A cat.</p>\n'
+  ], [
+    'Figure 1 is a cat.',
+    '<p>Figure 1 is a cat.</p>\n'
+  ], [
+    'Figure A is a cat.',
+    '<p>Figure A is a cat.</p>\n'
+  ], [
+    'Figure A A cat.',
+    '<p class="caption-img">Figure A A cat.</p>\n'
+  ], [
+    'Figure 1 A cat.',
+    '<p class="caption-img">Figure 1 A cat.</p>\n'
+  ], [
+    'Figure 1 a cat.',
+    '<p>Figure 1 a cat.</p>\n'
+  ], [
+    '図　猫',
+    '<p class="caption-img">図　猫</p>\n'
+  ], [
+    '図 猫',
+    '<p class="caption-img">図 猫</p>\n'
+  ], [
+    '図1　猫',
+    '<p class="caption-img">図1　猫</p>\n'
+  ], [
+    '図1.1 猫',
+    '<p class="caption-img">図1.1 猫</p>\n'
+  ], [
+    '図は猫',
+    '<p>図は猫</p>\n'
+  ] , [
+    '図1は猫',
+    '<p>図1は猫</p>\n'
+  ]
+];
+
+let n = 0;
+while(n < ms.length) {
+  const h = md.render(ms[n][0]);
+  try {
+    assert.strictEqual(h, ms[n][1]);
+  } catch(e) {
+    console.log('Incorrect: ')
+    console.log('M: ' + ms[n][0] + '\nH: ' + h +'C: ' + ms[n][1]);
+  };
+  n++;
+}
